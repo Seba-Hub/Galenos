c1a55f45c55242fe640ec150dfd309beb999d9f0
Sebastian <sebastianmartin.sg@gmail.com>
2023-12-09 19:07:07 -0300

    users

---
+++ galenos/usuarios/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Usuario
+from .serializers import UsuarioSerializer
+
+class UsuarioListCreateView(generics.ListCreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class UsuarioDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+
