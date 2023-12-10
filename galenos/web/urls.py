a9cb3f29285c7cd67fb7b39a4d66629ea539bda6
Sebastian <sebastianmartin.sg@gmail.com>
2023-12-10 13:00:40 -0300

    dsfs

--- galenos/web/urls.py
+++ galenos/web/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import inicio, login
+from .views import inicio, login, registro
 
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('login', login, name='login'),
+    path('registro', registro, name='registro'),
 ]
